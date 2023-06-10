3139195b091dae037a3c6465793f385e826e1aca
adm1 <guiraudjb@gmail.com>
2023-06-10 18:28:49 +0200

    mdification des bonus

--- arducible2023/jeux pour ordinateur ou téléphone/ArducibleDefold/main/Background.go
+++ arducible2023/jeux pour ordinateur ou téléphone/ArducibleDefold/main/Background.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/background.atlas\"\n"
-  "default_animation: \"1\"\n"
+  "default_animation: \"0\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
