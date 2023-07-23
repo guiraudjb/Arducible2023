c335a4cb7ad50acd39c50539fbf02e4cd08d4c9e
adm1 <guiraudjb@gmail.com>
2023-07-24 01:03:44 +0200

    maj jeu tanguy

--- arducible2023/jeux pour ordinateur ou téléphone/TanguyPeninShooting/GameObjects/labels/TimeLeftLabel.go
+++ arducible2023/jeux pour ordinateur ou téléphone/TanguyPeninShooting/GameObjects/labels/TimeLeftLabel.go
@@ -9,8 +9,8 @@ embedded_components {
   "}\n"
   "color {\n"
   "  x: 1.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
   "  w: 1.0\n"
   "}\n"
   "outline {\n"
@@ -36,6 +36,26 @@ embedded_components {
   ""
   position {
     x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/GameSprites.atlas\"\n"
+  "default_animation: \"M960x64\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 480.0
     y: 0.0
     z: 0.0
   }
