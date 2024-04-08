0862ae530de0412636b48f17a794cfcdaa099028
adm1 <adm1@localhost.localdomain>
2024-04-08 10:44:28 +0200

    maj atelier petanque

--- franck atelier pétanque/AtelierPétanquesimpliestArducibleDefold/GameObjects/labels/EndingMessage.go
+++ franck atelier pétanque/AtelierPétanquesimpliestArducibleDefold/GameObjects/labels/EndingMessage.go
@@ -64,13 +64,10 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"1000x800\"\n"
+  data: "tile_set: \"/assets/background.atlas\"\n"
+  "default_animation: \"0\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/assets/GameSprites.atlas\"\n"
-  "}\n"
   ""
   position {
     x: 0.0
@@ -183,13 +180,10 @@ embedded_components {
 embedded_components {
   id: "sprite1"
   type: "sprite"
-  data: "default_animation: \"rank1\"\n"
+  data: "tile_set: \"/assets/GameSprites.atlas\"\n"
+  "default_animation: \"rank1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/assets/GameSprites.atlas\"\n"
-  "}\n"
   ""
   position {
     x: 331.0
